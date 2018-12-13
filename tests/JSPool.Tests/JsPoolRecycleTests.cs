0aed2b5bfb143b8a5208f68b4d982b5a6ca6a6c7
Benjamin Batistic <benjamin.batistic@adacta-group.com>
2018-12-13 08:03:19 +0100

    Simulation of a JsPool recycling concurrency problem

---
+++ tests/JSPool.Tests/JsPoolRecycleTests.cs
@@ -0,0 +1,53 @@
+﻿using JavaScriptEngineSwitcher.Core;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JSPool.Tests
+{
+    public class JsPoolRecycleTests
+    {
+        [Fact]
+        public void ConcurentJsPoolRecyclingSimulationShouldNotThrow()
+        {
+            var pool = GetEnginePool();
+
+            void enginesConsumer()
+            {
+                int n = 1000;
+
+                while ((n--) > 0)
+                {
+                    pool.GetEngine();
+                }
+            }
+
+            void recycler()
+            {
+                int n = 1000;
+
+                while ((n--) > 0)
+                {
+                    pool.Recycle();
+                }
+            }
+
+            Parallel.Invoke(recycler, enginesConsumer, enginesConsumer, enginesConsumer, enginesConsumer, enginesConsumer, enginesConsumer, recycler);
+        }
+
+        public JsPool GetEnginePool()
+        {
+            var factory = new Mock<IEngineFactoryForMock>();
+            factory.Setup(x => x.EngineFactory()).Returns(new Mock<IJsEngine>().Object);
+            var config = new JsPoolConfig
+            {
+                StartEngines = 100,
+                EngineFactory = factory.Object.EngineFactory,
+            };
+
+            var pool = new JsPool(config);
+
+            return pool;
+        }
+    }
+}
